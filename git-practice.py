ac4e638c048b866b1093d70ec23a0e07696b598c
Andrea Colindres <colindresandrea284@gmail.com>
2023-07-21 21:46:58 -0600

    Tarea

---
+++ git-practice.py
@@ -0,0 +1,20 @@
+Describe que hacen los siguientes comandos de git, escribe su descripcion al frente en una sola linea.
+git status: Este comando se usa para obtener el estado actual del repositorio
+
+clon de git: apunta a un repositorio exitente y clona o copia dicho repositorio en un nuevo directorio, en otra ubicaión.
+
+tirar de git: se usa para eliminar archivos de un repositorio de git.
+
+pago de git: te permite desplazarte por las ramas creadas por git branch
+
+registro de git: explora el historial del repositorio.
+
+rama git: permite crear enumerar y eliminar ramas assi como cambiar su nombre.
+
+agregar: añade un cambio del directorio de trabajo en elk entorno del ensayo.
+
+git compromiso: captura una instantania de los cambios preparados en ese momento del proyecto.
+
+empujar git: empuja cualquier cambio o modificación.
+
+fusionar git: fisiona una o mas ramas dentro la rama que tienes activa.
