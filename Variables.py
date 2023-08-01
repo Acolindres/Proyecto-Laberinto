d0558ced00b720758335eaee04e21edf304da42c
Andrea Colindres <colindresandrea284@gmail.com>
2023-07-31 19:29:39 -0600

    tarea

---
+++ Variables.py
@@ -0,0 +1,12 @@
+print("Hola mundo")
+nombre = "Andrea"
+print('Hola, ' + nombre + '! Cómo estas?')
+nombre = "Andrea"
+print('Hola, {name}! Comó estas?' .format(name = nombre))
+print("En python los enteros tiene precisión ilimitada mientras que los números flotantes sí que tienen un límite en Python.")
+numero_1 = 54
+numero_2 = 32
+# suma
+print(numero_1 + numero_2)
+# imprimir
+print(nombre)
